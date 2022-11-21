0fe4dc3c53d3f64b3970d492462a1b741d896092
DiandraRifqiM <luxamsky@gmail.com>
2022-11-21 09:53:19 +0700

    P

---
+++ example-app/resources/views/Uji.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="Uji.css">
+    <link rel="stylesheet" href="{{ asset('css/Uji.css') }}">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>P</h1>
+</body>
+
+</html>
